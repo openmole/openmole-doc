2b486a82183db93bfb735c4cb81380f2809d59c7
Mathieu Leclaire <mathieu.leclaire@iscpif.fr>
2017-04-10 11:03:02 +0200

    Add Tabs

--- site/src/main/scala/org/openmole/doc/Site.scala
+++ site/src/main/scala/org/openmole/doc/Site.scala
@@ -1,4 +1,5 @@
 package site
+
 import scaladget.api.{BootstrapTags => bs}
 import scaladget.stylesheet.{all => sheet}
 
@@ -15,8 +16,26 @@ import bs._
 object Site extends JSApp {
 
   @JSExport()
-  def main(): Unit = {
-    div("OpenMOLE site")
+  def main(): Unit =
+    withBootstrapNative {
 
-  }
+      val div1 = div("Lorem ipsum dolor sit amet, " +
+        "consectetur adipiscing elit, sed do eiusmod tempor " +
+        "incididunt ut labore et dolore magna aliqua. " +
+        "Ut enim ad minim veniam, quis nostrud exercitation ullamco " +
+        "laboris nisi ut aliquip ex ea commodo consequat. Duis aute " +
+        "irure dolor in reprehenderit in voluptate velit esse cillum dolore " +
+        "eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, " +
+        "sunt in culpa qui officia deserunt mollit anim id est laborum.", padding := 10)
+
+
+      val div2 = vForm(bs.input("")(placeholder := "Name").render.withLabel("Your name"))
+
+      val tabs = Tabs(sheet.pills).
+        add("Scala Task", div1).
+        add("Docker Task", div2, true)
+
+
+      tabs.render
+    }
 }
